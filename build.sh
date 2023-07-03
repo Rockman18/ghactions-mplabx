7bef2126fab6e5bfed70f1bed23bbeabe0c84840
Rockman18 <rockman18.spam@gmail.com>
2023-07-03 16:21:34 +0200

    Remove output directory creation

--- build.sh
+++ build.sh
@@ -7,5 +7,5 @@ set -x -e
 prjMakefilesGenerator.sh $1@$2 || exit 1
 make -C $1 CONF=$2 build || exit 2
 
+echo "List generated files"
 tree /github/workspace/$1/dist
-cp -r /github/workspace/$1/dist /github/workspace/output 
