98d425e10a2cb6a3fb0afa87028c06c5e7d8a2ba
Rockman18 <rockman18.spam@gmail.com>
2023-10-21 00:54:05 +0200

    Allow multi-conf builds

--- build.sh
+++ build.sh
@@ -1,11 +1,9 @@
 #!/bin/sh
 
-echo "Docker Container Building $1:$2"
-
 set -x -e
 
-prjMakefilesGenerator.sh $1@$2 || exit 1
-make -C $1 CONF=$2 build || exit 2
-
-echo "List generated files"
-tree /github/workspace/$1/dist
+for conf in $(echo $2 | sed "s/,/ /g"); do
+  echo "Building project $1 with configuration $conf"
+  prjMakefilesGenerator.sh $1@$conf || exit 1
+  make -C $1 CONF=$conf build || exit 2
+done
